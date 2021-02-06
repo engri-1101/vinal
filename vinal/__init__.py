70bdf294effec4382a6e1c3fed95c33a4848130a
Henry Robbins <hwr26@cornell.edu>
2021-02-06 13:18:20 -0500

    Added docstring in __init__.py file.

--- vinal/__init__.py
+++ vinal/__init__.py
@@ -1,8 +1,27 @@
 """
-TMP
-====
+vinal (VIsualize Network ALgorithms)
+====================================
 
-TODO: Write description here.
+vinal is a Python package for visualizing graph/network algorithms. Currently,
+the following algorithms are implemented:
+
+- Shortest path problem
+    - Dijkstra's algorithm
+- Minimum Spanning Tree (MST)
+    - Prim's Algorithm
+    - Kruskal's Algorithm
+    - Reverse Kruskal's Algorithm
+- Travelling Salesman Problem (TSP)
+    - Random neighbor
+    - Nearest neighbor
+    - Nearest insertion
+    - Furthest insertion
+    - 2-OPT
+
+NetworkX graphs can be constructed from a single .csv file of node locations.
+Alternatively, one can specify edges of the graph by providing an additional
+.csv file of edges. The package relies on bokeh to generate standalone HTML
+files which can be viewed in a Jupyter Notebook inline or in a web browser.
 """
 
 __author__ = 'Henry Robbins'
