1630dab6427a5f30bc354b966a7a3a61deff094c
Henry Robbins <hwr26@cornell.edu>
2021-09-02 22:50:10 -0400

    Changed add_stylesheet to add_css_file in conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ html_theme_options = {
     'style_nav_header_background': '#b31b1b',
 }
 def setup(app):
-  app.add_stylesheet( "css/custom.css" )
+  app.add_css_file( "css/custom.css" )
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
